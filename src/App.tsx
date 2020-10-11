b48671b76c68d639ee53cb13ae8190431aba61b2
macedo-developer <macedorenata.developer@gmail.com>
2020-10-11 17:02:47 -0300

    Using Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
-  return <h1>Hi Lorena</h1>;
+  return (
+    <>
+      <h1>Hi Lorena</h1>
+
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
