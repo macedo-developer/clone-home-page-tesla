2b1826e04fb82aad3b21d2e4cc2a204b4f37aea7
macedo-developer <macedorenata.developer@gmail.com>
2020-10-11 17:10:29 -0300

    Using page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+
+import Page from "./components/Page";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
-      <h1>Hi Lorena</h1>
+      <Page />
 
       <GlobalStyles />
     </>
