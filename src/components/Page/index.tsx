8480dd5502e1284cf9f6e41c65e8405d74ff7b18
macedo-developer <macedorenata.developer@gmail.com>
2020-10-11 17:47:31 -0300

    Change space

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import DefaultOverlayContent from "../DefaultOverlayContent";
+import { ModelsWrapper, ModelSection } from "../Model";
 
 import { Container } from "./styles";
 
 const Page: React.FC = () => {
   return (
     <Container>
-      <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          <ModelSection
+            modelName="Model One"
+            overlayNode={
+              <DefaultOverlayContent
+                label="Model One"
+                description="Order online for delivery"
+              />
+            }
+          />
+        </div>
+      </ModelsWrapper>
     </Container>
   );
 };
